5386dfbbb14074844775525770125ec729065bb6
Stephen Okpalaononuju <stephenokpala@gmail.com>
2024-11-04 12:16:29 +0100

    chore: configure server timeout after 10 seconds

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
-import timeout from 'connect-timeout';
+import express, { Request, Response, NextFunction } from 'express'
+import timeout from 'connect-timeout'
 
 import routes from './routes'
 
@@ -7,28 +7,30 @@ const app = express()
 app.use(express.json())
 
 // Middleware to set timeout for all requests
-app.use(timeout('10s')); // Set timeout to 10 seconds
+app.use(timeout('10s')) // Set timeout to 10 seconds
 
 // Middleware to handle timed-out requests
 app.use((req: Request, res: Response, next: NextFunction) => {
-    if (!req.timedout) next(); // If request did not time out, proceed
-});
+    if (!req.timedout) next() // If request did not time out, proceed
+})
 
 // Error handler for timed-out requests
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err && req.timedout) {
-        res.status(408).json({ message: 'Request timed out. Check your network connection.' });
+        res.status(408).json({
+            message: 'Request timed out. Check your network connection.',
+        })
         return
     } else {
-        next(err);
+        next(err)
     }
-});
+})
 
 // Software accessibility test
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({message: 'Welcome to Demo Credit!'});
+    res.status(200).json({ message: 'Welcome to Demo Credit!' })
     return
-});
+})
 
 //Expose routes
 app.use('/api/v1', routes)
